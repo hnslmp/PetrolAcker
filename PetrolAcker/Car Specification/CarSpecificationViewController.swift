44c3c5a68b80ba336100dae3cebe8857aed957e7
hnslmp <ancemo14@gmail.com>
2022-07-21 17:59:45 +0700

    feat: all ui done + grouping

---
+++ PetrolAcker/Car Specification/CarSpecificationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CarSpecificationViewController.swift
+//  PetrolAcker
+//
+//  Created by Hansel Matthew on 21/07/22.
+//
+
+import UIKit
+
+class CarSpecificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+}
---
+++ PetrolAcker/History/HistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryTableViewCell.swift
+//  PetrolAcker
+//
+//  Created by Hansel Matthew on 21/07/22.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
