5ca76f3d7b71345c09121f229f00e20e80f2e16e
hnslmp <ancemo14@gmail.com>
2022-08-04 16:06:20 +0700

    feat: CarModel

---
+++ PetrolAcker/CarSpecification/CarModel.swift
@@ -0,0 +1,14 @@
+//
+//  CarModel.swift
+//  PetrolAcker
+//
+//  Created by Hansel Matthew on 22/07/22.
+//
+
+import Foundation
+
+struct Car{
+    var carName: String
+    var fuelConsumption: Int
+    var fuelTankCapacity: Int
+}
